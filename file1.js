8b0530436767396b017fd1b01bc71d455ee93b45
gagan257 <arorag2577@gmail.com>
2023-01-30 23:47:51 +0530

    GET req

--- file1.js
+++ file1.js
@@ -1,18 +1,18 @@
-// const lib = require('./file2.js'); // use to define other file in this file(Import)
-// console.log(lib);
-// lib.printFunction();//using function from other file
+const lib = require('./file2.js'); // use to define other file in this file(Import)
+console.log(lib);
+lib.printFunction();//using function from other file
 
-// const fs = require('fs')// inbuild lib
-// fs.writeFile('myfile.txt',"some data goes here", function(err){//creating file with given name and data
-//     // writeFile('filename',"data","callback function")
-//     if(err) throw err
+const fs = require('fs')// inbuild lib
+fs.writeFile('myfile.txt',"some data goes here", function(err){//creating file with given name and data
+    // writeFile('filename',"data","callback function")
+    if(err) throw err
 
-//     console.log("file work DONE!")
-// })
-/////////////////////////////////////////////////////////////
-// const fs = require('fs')
-// fs.writeFile('/myfile.txt',function(err,data){
-//     if (err) throw err
+    console.log("file work DONE!")
+})
+///////////////////////////////////////////////////////////
+const fs = require('fs')
+fs.writeFile('/myfile.txt',function(err,data){
+    if (err) throw err
 
-//     console.log(data.toString())
-// })
+    console.log(data.toString())
+})
--- server.js
+++ server.js
@@ -10,6 +10,19 @@ app.get('/wish',(req,res)=>{//for server link with localhost:portnum/wish)
     res.send('<h1 style="color: green;">Good Morning</h1>')
 })
 
+app.get('/form',(req,res)=>{
+    console.log(__dirname)
+    res.sendFile('files/form.html', {root: __dirname})
+})
+
+app.get('/greet',(req,res)=>{
+    let person = 'Guest'
+    if(req.query.person) person = req.query.person//in general req it does not exists
+    res.send('Good Morning ' + person)
+    // example: http://localhost:4444/greet?person=gagan = good morning gagan
+    // example: http://localhost:4444/greet = good morning
+})
+
 app.listen(4444,()=>{
     console.log('server started on http://localhost:4444')
 })
